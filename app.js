2cd25d609620e45633dfc63226f6f4da35a09b52
1607Suraj001 <surajbhardwaj1607@gmail.com>
2019-02-25 15:31:41 +0530

    intial_commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const EXPRESS = require('express')
+const SOCKET = require('socket.io')
+//app setup
+let port = 4000;
+
+let app = EXPRESS();
+
+const SERVER = app.listen(port,function(){
+    console.log('Server started at port ',port);
+})
+
+//Static Files 
+
+app.use(EXPRESS.static('public'));
+
+//Socket setup
+
+var io = SOCKET(SERVER);
+
+io.on('connection',function(socket){
+    console.log('made socket connection',socket.id)
+
+    socket.on('chat',function(data){
+        io.sockets.emit('chat',data)
+    })
+})
